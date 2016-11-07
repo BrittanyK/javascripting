14fb0cc535943f4077f2bbaa4b3c0e9432b1d3ff
MKH103-18 <MKH103-18@lbcc.linnbenton.edu>
2016-11-07 09:30:08 -0800

    Finished if-statement.js

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+i = 5;
+if( i < fruit.length){
+    console.log('The fruit name has more than five characters.');
+}
+else {
+    console.log('The fruit name has five characters or less.');
+}
